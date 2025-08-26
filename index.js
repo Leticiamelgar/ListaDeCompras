3e54e942a0f0eeeffa080591bae4f86bbfd81a22
Leticia Melgar <leticia.melgar@estudante.ufjf.br>
2025-08-25 23:50:16 -0300

    fim

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+import verificaListaVazia from "./scripts/verificaListaVazia.js";
+
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar =document.getElementById("adicionar-item");
 
@@ -7,19 +9,9 @@ botaoAdicionar.addEventListener("click", (evento)=> {
     evento.preventDefault();
     const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista)
-    verificaListaVazia();
+    verificaListaVazia(listaDeCompras);
 })
 
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
 
-function verificaListaVazia(){
-    const itensDaLista = listaDeCompras.querySelectorAll("li");
-    if(itensDaLista.length===0){
-        mensagemListaVazia.style.display = "block"
-    }
-    else {
-        mensagemListaVazia.style.display = "none"
-    }
-}
 
-verificaListaVazia();
+verificaListaVazia(listaDeCompras);
